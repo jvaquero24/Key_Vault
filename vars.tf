d15af39b38a4e5ff2480d8b2428dba442669f110
jvaquero24 <67736596+jvaquero24@users.noreply.github.com>
2020-10-16 02:53:23 +0200

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,11 @@
+variable keyvault_name {
+  description = "Nombre del Key Vault"
+}
+
+variable databrick_name {
+  description = "Nombre del Databrick"
+}
+
+variable databricks_secret_value {
+  description = "Access token generado en Databricks que se añadirá en el KV"
+} 
