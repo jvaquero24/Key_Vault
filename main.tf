95f874bee6499887077b74163bdd90bc771bb6c2
jvaquero24 <67736596+jvaquero24@users.noreply.github.com>
2020-10-16 10:45:46 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,40 +2,10 @@ data "azurerm_resource_group" "RG" {
   name = var.resource_group_name
 }
 
-data "azurerm_client_config" "current" {}
-
-
-resource "azurerm_key_vault" "AKV" {
-  name                        = "testvault"
-  location                    = var.location
-  resource_group_name         = data.azurerm_resource_group.RG.name
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_enabled         = true
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
-
-  sku_name = var.sku_keyvault
-
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    key_permissions = [
-      "get",
-    ]
-
-    secret_permissions = [
-      "get",
-    ]
-
-    storage_permissions = [
-      "get",
-    ]
-  }
-
-  network_acls {
-    default_action = "Deny"
-    bypass         = "AzureServices"
-  }
+resource"azurerm_key_vault""KEYVAULT" {
+  name                = var.name_keyvault
+  location            = var.location
+  resource_group_name = data.azurerm_resource_group.RG.name
+  tenant_id           = var.tenant_id
+  sku_name            = var.sku_keyvault
 }
